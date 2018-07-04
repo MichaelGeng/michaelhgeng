2abf644b365c161623cc5ebb0dc1b29ebebc7596
unknown <user.name>
2018-07-04 06:43:57 -0700

    Added Bootstrap Buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
 
 class Buttons extends Component {
   render() {
     return (
       <div>
-        <button type="button">Bio</button>
-        <button type="button">Contact</button>
-        <button type="button">Technical Abilities</button>
+        <ButtonToolbar>
+          {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+          <Button bsStyle="primary">Bio</Button>
+
+          {/* Indicates a successful or positive action */}
+          <Button bsStyle="success">Experience</Button>
+
+          {/* Contextual button for informational alert messages */}
+          <Button bsStyle="info">Skills</Button>
+
+          {/* Indicates caution should be taken with this action */}
+          <Button bsStyle="warning">Contact</Button>
+        </ButtonToolbar>;
       </div>
     )
   }
