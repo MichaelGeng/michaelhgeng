4d22401712ed0c9a5b61a059c99c3cf76102a038
unknown <user.name>
2018-07-06 13:54:37 -0700

    Updated bio text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,12 +7,12 @@ class Bio extends Component {
     return (
       <div>
           <img src = {photo} id = "selfie" alt="me"/>
-          <p class="about_para"> I'm a Junior in Computer Science and Entrepreneurship at the University of Michigan. Since my first programming course, I have developed and continue to improve my foundation in data structures and algorithms. My passion lies in Ofinding opportunities to apply skills I have learned to create, implement, and solve complex computer science problems businesses face today. After finding a solution, I shift my mindset towards optimizations. How can I make my current program faster, more elegant, and more readable? Making continuous upgrades and seeing the performance improvements is one of my favorite aspects of development.</p>
-          
-          <p class="about_para"> Outside of programming I enjoy working out, traveling, and trying new foods. I am passionate about fitness and the progressive mindset it has given me. Fitness is a break in the day where I can clear my head, unwind, and reset mentally. The realization of pushing my body to perform feats it once couldn't reminds me that hard work goes a long way. This mentality has transferred over into other areas of my life such as taking care of my personal relationships with friends and family and putting my best foot forward in my profession.</p>
-          
-          <p class = "about_para"> I love to travel to new locations and sample the local cuisines! I have been fortunate enough to have visited many areas in the U.S and China. Some of my favorite foods I have eaten include Korean Barbeque in Los Angeles, Pizza in New York, and fresh Dumplings in Shanghai. I'm hoping to visit Europe in the near future and continue to explore more of New York's food culture.</p>
+          <p class="about_para"> I'm a rising Senior in Computer Science at the University of Michigan. Currently seeking fulltime positions begininning in 2019 after my graduation.</p>
 
+          <p class="about_para">I have experience with fullstack software development with an emphasis on frontend development.</p>
+          <p class="about_para">Since my first programming course, I have developed and continue to improve my foundation in data structures and algorithms. My passion lies in finding opportunities to apply skills I have learned to create, implement, and solve complex computer science problems that businesses face today.</p>
+          <p class="about_para">I continuously seek opportunities to learn and enhance my skills both technically and interpersonally to contribute value in my profession.</p>
+          <p class="about_para"> Outside of programming I enjoy working out, traveling, gaming, browsing yelp for new food spots, and visiting national parks</p>
       </div>
     )
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react'
 import { Button, ButtonToolbar } from 'react-bootstrap';
 
-
 class Buttons extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      bio: false,
+      exp: false
+    };
+    this.handleBioClick = this.handleBioClick.bind(this);
+    this.handleExpClick = this.handleExpClick.bind(this);
+  }
+    handleBioClick(){
+      this.setState(previousState => {
+        return {
+          bio: !previousState.bio
+        };
+      });
+    }
+
+    handleExpClick(){
+      this.setState(previousState => {
+        return {
+          exp: !previousState.exp
+        };
+      });
+    }
+
   render() {
     return (
       <div>
         <ButtonToolbar>
           {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-          <Button bsStyle="primary">Bio</Button>
+          <Button bsStyle="primary" onClick={this.handleBioClick.bind(this)} active>
+            {this.state.bio ? "show" : "not show"}
+          </Button>
 
           {/* Indicates a successful or positive action */}
-          <Button bsStyle="success">Experience</Button>
+          <Button bsStyle="success" onClick={this.handleExpClick.bind(this)} active>
+            {this.state.exp ? "exp" : "not exp"}
+          </Button>
 
           {/* Contextual button for informational alert messages */}
-          <Button bsStyle="info">Skills</Button>
+          <Button bsStyle="info" active>Skills</Button>
 
           {/* Indicates caution should be taken with this action */}
-          <Button bsStyle="warning">Contact</Button>
-        </ButtonToolbar>;
+          <Button bsStyle="warning" active>Contact</Button>
+        </ButtonToolbar>
       </div>
     )
   }
