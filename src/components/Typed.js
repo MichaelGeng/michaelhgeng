f99a6e7055692b627f753f45099757a676bf9732
MichaelGeng <mikegeng@umich.edu>
2018-11-12 17:49:10 -0500

    Updated typed messages

--- src/components/Typed.js
+++ src/components/Typed.js
@@ -7,12 +7,12 @@ class Typed extends Component {
   render() {
     return (
       <Typist>
-        <span>Hi, I'm Michael.</span>
-        <Typist.Backspace count={16} delay={200} />
-        <span>A rising Senior studying Computer Science at the University of Michigan.</span>
-        <Typist.Backspace count={72} delay={200} />
-        <span>Looking for positions particularly in Front-End Development or Full-Stack.</span>
-        <Typist.Backspace count={75} delay={200} />
+        <span>Hi, I'm Michael Geng.</span>
+        <Typist.Backspace count={21} delay={200} />
+        <span>Software Engineer at Nike interested in Web</span>
+        <Typist.Backspace count={43} delay={200} />
+        <span>JavaScript and Python are my favorite tools</span>
+        <Typist.Backspace count={43} delay={200} />
         <span>Thanks for stopping by!</span>
       </Typist>
     );
