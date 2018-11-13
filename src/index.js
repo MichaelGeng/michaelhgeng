7b6d1ce20df21373eb835e571d37d2e6c1e00d9d
MichaelGeng <mikegeng@umich.edu>
2018-11-12 21:01:18 -0500

    Remove Quote from index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typed from './components/Typed';
-import Quote from './components/Quote';
 import Body from './components/Body';
 
-ReactDOM.render(<Quote />, document.getElementById('quote'));
 ReactDOM.render(<Typed />, document.getElementById('typed'));
 ReactDOM.render(<Body />, document.getElementById('body'))
