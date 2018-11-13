d7f4a2c2107492b7a685320177cc1b3654e1f8bd
MichaelGeng <mikegeng@umich.edu>
2018-11-12 21:02:35 -0500

    Changed to relative image paths

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import './About.css'
 import Favicon from 'react-favicon';
-const photo = require('../images/hike_dogmt.jpg')
-const favicon = require('../images/MG.png')
 
 class About extends Component {
   render() {
     return (
       <div>
-          <Favicon url = {favicon} />
-          <img src = {photo} id = "selfie" alt="me"/>
+          <Favicon url = "/images/MG.png" />
+          <img src = "/images/hike_dogmt.jpg" id = "selfie" alt="me"/>
           <p> I will be graduating in December 2018 with a Bachelors in Science 
               in Computer Science from the University of Michigan. Afterwards I 
               will be a Software Engineer I at Nike in Portland, Oregon.</p>
