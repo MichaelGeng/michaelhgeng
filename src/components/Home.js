4ad5ccb3474dee6a8b99e3cba112af36a3a983ae
MichaelGeng <mikegeng@umich.edu>
2018-11-13 13:17:41 -0500

    Updated reconstruction date

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
   render() {
     return (
         <div id="home">
-            <p><b>This site is currently undergoing reconstruction 11/12/18</b></p>
+            <p><b>This site is currently undergoing reconstruction 11/13/18</b></p>
             <Quote />
         </div>
     );
