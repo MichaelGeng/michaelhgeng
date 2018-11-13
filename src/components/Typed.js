9e162c3e5df93bf754ab0e0a17d82bfa3eddf79a
MichaelGeng <mikegeng@umich.edu>
2018-11-13 13:16:51 -0500

    Shortened Text

--- src/components/Typed.js
+++ src/components/Typed.js
@@ -7,13 +7,10 @@ class Typed extends Component {
   render() {
     return (
       <Typist>
-        <span>Hi, I'm Michael Geng.</span>
+        <span>Hi, I'm Michael.</span>
         <Typist.Backspace count={21} delay={200} />
-        <span>Software Engineer at Nike interested in Web</span>
-        <Typist.Backspace count={43} delay={200} />
-        <span>JavaScript and Python are my favorite tools</span>
-        <Typist.Backspace count={43} delay={200} />
-        <span>Thanks for stopping by!</span>
+        <span>Thanks for visiting!</span>
+        <Typist.Backspace count={20} delay={300} />
       </Typist>
     );
   }
