a17c14635a614761edbf980678ada0e62d970736
MichaelGeng <mikegeng@umich.edu>
2018-11-13 13:16:39 -0500

    Removed Type div from index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Typed from './components/Typed';
 import Body from './components/Body';
 
-ReactDOM.render(<Typed />, document.getElementById('typed'));
 ReactDOM.render(<Body />, document.getElementById('body'))
