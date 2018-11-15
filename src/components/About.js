63582e1b27d0854805ebc318db117de7e17266e2
MichaelGeng <mikegeng@umich.edu>
2018-11-14 20:55:33 -0500

    Added two p tags to bio

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,11 @@ class About extends Component {
           <p> I will be graduating in December 2018 with a Bachelors in Science 
               in Computer Science from the University of Michigan. Afterwards I 
               will be a Software Engineer I at Nike in Portland, Oregon.</p>
+
+          <p>I'm highly interested in Web Development and have experience in both
+             Front-End and Back-End Development primarily in JavaScript and Python.
+          </p>
+          <p>Other interests include hiking, fitness, and being a yelper!</p>
       </div>
     )
   }
