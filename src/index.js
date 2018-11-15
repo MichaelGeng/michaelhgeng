2ccadedc3fd1250acdb0bc96407f94df1c484cde
MichaelGeng <mikegeng@umich.edu>
2018-11-14 19:57:23 -0500

    Added bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Body from './components/Body';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Body />, document.getElementById('body'))
