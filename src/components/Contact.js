92b10b56eefea0933eee306e5a3c122b6f7a3e6b
MichaelGeng <mikegeng@umich.edu>
2018-11-16 22:37:08 -0500

    Using new contact structure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,13 @@ import './Contact.css';
 class Contact extends Component {
   render() {
     return (
-        <ul id = "social">
-            <li class = "sociali"><a href="https://www.linkedin.com/in/michael-geng/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            <li class = "sociali"><a href="mailto:mikegeng@umich.edu"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
-            <li class = "sociali"><a href="https://github.com/MichaelGeng"><i class="fa fa-github" aria-hidden="true"></i></a></li>
-            <li class = "sociali"><a href="tel:734-546-5913"><i class="fa fa-phone" aria-hidden="true"></i></a></li>
-        </ul>
+      <div class="middle">
+        <a class="contact_btn" href="#"><i class="fab fa-facebook-f"></i></a>
+        <a class="contact_btn" href="#"><i class="fab fa-twitter"></i></a>
+        <a class="contact_btn" href="#"><i class="fab fa-google"></i></a>
+        <a class="contact_btn" href="#"><i class="fab fa-instagram"></i></a>
+        <a class="contact_btn" href="#"><i class="fab fa-youtube"></i></a>
+      </div>
     );
   }
 }
