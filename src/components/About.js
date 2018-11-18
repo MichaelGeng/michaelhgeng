2198017f0532c2be9cbcffa60744165c71dcde16
MichaelGeng <mikegeng@umich.edu>
2018-11-17 22:53:32 -0500

    Added textfit

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactFitText from 'react-fittext'
 import './About.css'
 
 class About extends Component {
@@ -6,14 +7,20 @@ class About extends Component {
     return (
       <div>
           <img src = "/images/hike_dogmt.jpg" id = "selfie" alt="me"/>
-          <p> I will be graduating in December 2018 with a Bachelors in Science 
-              in Computer Science from the University of Michigan. Afterwards I 
-              will be a Software Engineer I at Nike in Portland, Oregon.</p>
+          <ReactFitText minFontSize = {12} maxFontSize={15}>
+            <p> I will be graduating in December 2018 with a Bachelors in Science 
+                in Computer Science from the University of Michigan. Afterwards I 
+                will be a Software Engineer I at Nike in Portland, Oregon.</p>
+          </ReactFitText>
 
-          <p>I'm highly interested in Web Development and have experience in both
-             Front-End and Back-End Development primarily in JavaScript and Python.
-          </p>
-          <p>Other interests include hiking, fitness, and being a yelper!</p>
+          <ReactFitText minFontSize = {12} maxFontSize={15}>
+            <p>I'm highly interested in Web Development and have experience in both
+              Front-End and Back-End Development primarily in JavaScript and Python.</p>
+          </ReactFitText>
+
+          <ReactFitText minFontSize = {12} maxFontSize={15}>
+            <p>Other interests include hiking, fitness, and being a yelper!</p>
+          </ReactFitText>
       </div>
     )
   }
