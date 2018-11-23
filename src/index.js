115a67e8b8dbbab5b2c8ece0cb566d772a67cd4f
MichaelGeng <mikegeng@umich.edu>
2018-11-23 18:57:43 -0500

    convert body to .jsx file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Body from './components/Body';
+import Body from './components/Body.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Body />, document.getElementById('body'))
