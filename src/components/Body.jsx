f3efbed68e88b3b1475261e3a3fd76d02fd38cc2
MichaelGeng <mikegeng@umich.edu>
2018-11-23 18:57:57 -0500

    Convert to jsx file

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,16 +9,12 @@ import './Body.css';
 import { Button } from 'reactstrap';
 import Favicon from 'react-favicon';
 
-
 class Body extends Component {
     constructor() {
         super();
-        this.state = { show_intro: true, show_home: true, show_about: false, show_resume: false, show_contact: false };
+        this.state = { show_intro: true, show_home: "active home", show_about: "about" };
 
-        this.handleHomeClick = this.handleHomeClick.bind(this);
-        this.handleStoryClick = this.handleStoryClick.bind(this);
-        this.handleResumeClick = this.handleResumeClick.bind(this);
-        this.handleContactClick = this.handleContactClick.bind(this);
+        this.navClick = this.navClick.bind(this);
     }
 
     componentDidMount() {
@@ -27,26 +23,34 @@ class Body extends Component {
         }, 7000);
       }
 
-    handleHomeClick(){
-        this.setState({show_home: true, show_about: false, show_resume: false, show_contact: false});
-    }
-    handleStoryClick(){
-        this.setState({show_home: false, show_about: true, show_resume: false, show_contact: false});
-    }
-    handleResumeClick(){
-        this.setState({show_home: false, show_about: false, show_resume: true, show_contact: false});
-    }
-    handleContactClick(){
-        this.setState({show_home: false, show_about: false, show_resume: false, show_contact: true});
+    navClick(e){
+        console.log('clicked on about');
+
+        console.log(e.target.id);
+
+        if (e.target.id === this.state.show_home){
+            console.log('home is active');
+            this.setState({show_home: "active home", show_about: "about"});
+        } else {
+            this.setState({show_home: "home", show_about: "active about"});
+        }
     }
     render() {
+        let home = this.state.show_home;
+        let about = this.state.show_about;
         return (
             <div id = "parent">
                 <Favicon url = "/images/MG.png" />
                 <div id = "intro">{ this.state.show_intro ? <Typed /> : null }</div>
-                <Delay wait={1000}>
+                 {/* <Delay wait={1000}> */}
+                 <ul>
+                        <li><span id={this.state.show_home} onClick={(e) => {this.navClick(e)}}>Home</span></li>
+                        <li><span id={this.state.show_about} onClick={(e) => {this.navClick(e)}}>About</span></li>
+                </ul>
+                 <div id = "bodyContainer">
+
                     <div id = "buttons">
-                            <Button style={{"height": "100%", "width": "23%", "minWidth": "46px"}} onClick={this.handleHomeClick} outline color="primary">Home</Button>{' '}
+                            <Button style={{"height": "100%", "width": "23%", "minWidth": "46px"}} onClick={this.handleHomeClick} outline color="primary" active>Home</Button>{' '}
                             <Button style={{"height": "100%", "width": "23%", "minWidth": "46px"}} onClick={this.handleStoryClick} outline color="secondary">About</Button>{' '}
                             <Button style={{"height": "100%", "width": "23%", "minWidth": "46px"}} onClick={this.handleResumeClick} outline color="warning">Resume</Button>{' '}
                             <Button style={{"height": "100%", "width": "23%", "minWidth": "46px"}} onClick={this.handleContactClick} outline color="success">Contact</Button>{' '}
@@ -57,7 +61,8 @@ class Body extends Component {
                         { this.state.show_resume ? <Chart /> : null}
                         { this.state.show_contact ? <Contact /> : null}
                     </div>   
-                </Delay>         
+                {/* </Delay> */}
+                </div>        
             </div>
         )
     }
