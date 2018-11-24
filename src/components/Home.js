6df8d36b8f5d20cc8a3412589f9f18f049e953f0
MichaelGeng <mikegeng@umich.edu>
2018-11-24 14:35:58 -0500

    Updated construction date

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
   render() {
     return (
         <div id="home">
-            <p><b>This site is currently undergoing reconstruction 11/23/18</b></p>
+            <p><b>This site is currently undergoing reconstruction 11/25/18. I expect to be done within 2 weeks.</b></p>
             <p>This website is currently being reimplented with React.</p>
             <p>Source code can be found on my <a href="https://github.com/MichaelGeng/michaelhgeng">GitHub</a>.</p>
             <Quote />
