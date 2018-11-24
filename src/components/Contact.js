fccb18eade112ef7ceda71c3d30c1ffdbffe5bae
MichaelGeng <mikegeng@umich.edu>
2018-11-23 21:45:49 -0500

    rename class to className

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,11 +7,11 @@ class Contact extends Component {
     return (
       <div>
         <p>Please reach out at mikegeng@umich.edu for conversation!</p>
-        <a class="contact_btn" href="https://github.com/MichaelGeng"><i class="fab fa-github"></i></a>
-        <a class="contact_btn" href="https://www.linkedin.com/in/michael-geng/"><i class="fab fa-linkedin-in"></i></a>
-        <a class="contact_btn" href="mailto:mikegeng@umich.edu"><i class="fas fa-envelope"></i></a>
-        <a class="contact_btn" href="tel:734-546-5913"><i class="fas fa-phone"></i></a>
-        <a class="contact_btn" href="https://www.instagram.com/mikeegeng/"><i class="fab fa-instagram"></i></a>
+        <a className="contact_btn" href="https://github.com/MichaelGeng"><i className="fab fa-github"></i></a>
+        <a className="contact_btn" href="https://www.linkedin.com/in/michael-geng/"><i className="fab fa-linkedin-in"></i></a>
+        <a className="contact_btn" href="mailto:mikegeng@umich.edu"><i className="fas fa-envelope"></i></a>
+        <a className="contact_btn" href="tel:734-546-5913"><i className="fas fa-phone"></i></a>
+        <a className="contact_btn" href="https://www.instagram.com/mikeegeng/"><i className="fab fa-instagram"></i></a>
       </div>
     );
   }
