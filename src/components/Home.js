b087a728859a078414590485e2108a77939a6e54
MichaelGeng <mikegeng@umich.edu>
2018-11-25 23:06:51 -0500

    Added home screen picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ class Home extends Component {
   render() {
     return (
         <div id="home">
+            <img src = "/images/home_pic.jpg" id = "selfie" alt="me"/>
             <p><b>This site is currently undergoing reconstruction 11/26/18. I expect to be done within 2 weeks.</b></p>
             <p>This website is currently being reimplented with React.</p>
             <p>Source code can be found on my <a href="https://github.com/MichaelGeng/michaelhgeng">GitHub</a>.</p>
