834684e0c3afc96b68aefeb0904d71dd8147e042
MichaelGeng <mikegeng@umich.edu>
2018-11-26 21:37:32 -0500

    Edited Text Colors

--- src/components/About.js
+++ src/components/About.js
@@ -13,22 +13,26 @@ class About extends Component {
             <Col xs="12" sm="3" className="left">ABOUT ME</Col>
             <Col xs="12" sm="9">
               <ReactFitText minFontSize = {12} maxFontSize={15}>
-                <p>In Winter 2018, I will be graduating from the University of Michigan with a B.S. 
-                   in Computer Science. Starting February 2019, I will begin my career as a Software Engineer I at Nike in Portland, Oregon.</p>
+                <p>In Spring 2019, I will begin my career as a Software Engineer I at Nike in Portland, Oregon. Team placement is TBD within Nike Digital.</p>
               </ReactFitText>
 
               <ReactFitText minFontSize = {12} maxFontSize={15}>
-                <p>In Summer 2018, I worked at Nike as an Application Engineer Intern on the Rapid Application Development Team. I 
+                <p>In Winter 2018, I will be graduating from the University of Michigan with a B.S. 
+                   in Computer Science. My coursework heavily revolved around Data Structures, Algorithms, Software Engineering, and Web Development.</p>
+              </ReactFitText>
+
+              <ReactFitText minFontSize = {12} maxFontSize={15}>
+                <p>In Summer 2018, I worked at <a href="https://jobs.nike.com/digital">Nike</a> as an Application Engineer Intern on the Rapid Application Development Team. I 
                    implemented a Slack Bot in Python to aid with daily stand-ups and worked on user interface engineering with React.</p>
               </ReactFitText>
 
               <ReactFitText minFontSize = {12} maxFontSize={15}>
-                <p>In Summer 2017, I worked at General Motors Financial as an IT Special Projects Intern on the international IT Team. I
+                <p>In Summer 2017, I worked at <a href="https://www.gmfinancial.com/">General Motors Financial</a> as an IT Special Projects Intern on the international IT Team. I
                   worked with a team of developers and business analysts to create responsive web pages primarily using jQuery.</p>
               </ReactFitText>
 
               <ReactFitText minFontSize = {12} maxFontSize={15}>
-                <p>In Summer 2016, I worked at Advantage Computing Systems as a Systems Analyst Intern on the Client Services Team. I
+                <p>In Summer 2016, I worked at <a href="https://www.advantagecs.com/">Advantage Computing Systems</a> as a Systems Analyst Intern on the Client Services Team. I
                   worked with developers, business analysts, and VP's to aid in large-scale data migration using MySQL. I also worked
                   on automating time-off requests with Python, eliminating paper usage and saving time.</p>
               </ReactFitText>
@@ -39,16 +43,16 @@ class About extends Component {
             <Col xs="12" sm="3" className="left">SKILLS</Col>
             <Col xs="12" sm="9">
               <Row>
-                <Col xs="12" sm="3">Programming Languages
+                <Col xs="12" sm="3"><div class="skill_section">Programming</div>
                   <p>JavaScript <br/>Python<br/>C++<br/>SQL/NoSQL<br/>HTML/CSS</p>
                 </Col>
-                <Col xs="12" sm="3">Frameworks
+                <Col xs="12" sm="3"><div class="skill_section">Frameworks</div>
                   <p>React<br/>Node<br/>jQuery<br/>Flask</p>
                 </Col>
-                <Col xs="12" sm="3">Databases
+                <Col xs="12" sm="3"><div class="skill_section">Databases</div>
                   <p>SQLite<br/>MySQL<br/>MongoDB<br/>DynamoDB</p>
                 </Col>
-                <Col xs="12" sm="3">Libraries/Tools
+                <Col xs="12" sm="3"><div class="skill_section">Libraries/Tools</div>
                   <p>Git<br/>Redux<br/>Webpack<br/>Babel<br/>Rest APIs<br/>AWS</p>
                 </Col>
               </Row>
