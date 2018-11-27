cd0a0268d04ec26d083a238f9c1c4894083dd46f
MichaelGeng <mikegeng@umich.edu>
2018-11-26 21:15:51 -0500

    Removed Github/IG reference

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,11 +7,9 @@ class Contact extends Component {
     return (
       <div>
         <p>Please reach out at mikegeng@umich.edu for conversation!</p>
-        <a className="contact_btn" href="https://github.com/MichaelGeng"><i className="fab fa-github"></i></a>
         <a className="contact_btn" href="https://www.linkedin.com/in/michael-geng/"><i className="fab fa-linkedin-in"></i></a>
         <a className="contact_btn" href="mailto:mikegeng@umich.edu"><i className="fas fa-envelope"></i></a>
         <a className="contact_btn" href="tel:734-546-5913"><i className="fas fa-phone"></i></a>
-        <a className="contact_btn" href="https://www.instagram.com/mikeegeng/"><i className="fab fa-instagram"></i></a>
       </div>
     );
   }
