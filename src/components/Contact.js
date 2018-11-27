a560023ab87b68c808cf1e090791a90eb702b296
MichaelGeng <mikegeng@umich.edu>
2018-11-26 21:38:00 -0500

    Added contact section div id

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,7 @@ import './Contact.css';
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <p>Please reach out at mikegeng@umich.edu for conversation!</p>
+      <div id="contact_section">
         <a className="contact_btn" href="https://www.linkedin.com/in/michael-geng/"><i className="fab fa-linkedin-in"></i></a>
         <a className="contact_btn" href="mailto:mikegeng@umich.edu"><i className="fas fa-envelope"></i></a>
         <a className="contact_btn" href="tel:734-546-5913"><i className="fas fa-phone"></i></a>
